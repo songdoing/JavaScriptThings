ced254f4533dd447cedc090f3f9a86832f971fdc
wonjinsong <msmk22239@gmail.com>
2020-11-23 14:39:40 -0500

    complete

--- calculator/calc.js
+++ calculator/calc.js
@@ -4,9 +4,66 @@ input.getInput = function() {
     return this.inputArr.join("");
 };
 
+input.removeAll = function(val) {
+    this.inputArr = [];
+    this.inputArr.push(val);
+};
+
+input.empty = function() {
+    return this.inputArr.length === 0;
+};
+
+input.getValue = function() {
+    var str = this.inputArr.shift();
+    var n = Number(str);
+    return n;
+};
+
+input.prepareCalculate = function() {
+    this.inputArr = this.inputArr.join("").split(" ");
+};
+
+input.getOperator = function() {
+    var op = this.inputArr.shift();
+    if(op === '+' || op === '-' || op === '×' || op === '÷') {
+         return op;           
+    }else {
+        return "$";
+    }   
+};
+
 var output = {};
 output.text = document.getElementById('output');
+output.print = function(str) {
+    this.text.innerHTML=str;
+};
+output.display = function() {
+    this.text.innerHTML = input.getInput();
+};
 
+var calculator = {};
+        calculator.calculate = function(first, second, op){
+            var result = 0;
+            switch(op) {
+                case "+" :
+                result = first + second;
+                break;
+                case "-" :
+                result = first - second;
+                break;
+                case "×" :
+                result = first * second;
+                break;
+                case "÷" :
+                result = first / second;
+                break;
+                default : 
+                return NaN;
+            }
+            return result;
+
+        };
+        
 var clickButtons = function(event) {
     var str = event.target.innerHTML;
 
@@ -14,30 +71,34 @@ var clickButtons = function(event) {
         case 'C' :
             input.inputArr.pop();
         break;
-        case '+' :
-            input.inputArr.push(' ' + str + ' ');
-        break;
-        case '-' :
-            input.inputArr.push(' ' + str + ' ');
-        break;
-        case '×' :
-            input.inputArr.push(' ' + str + ' ');
-        break;
+        case '+' :       
+        case '-' :         
+        case '×' :            
         case '÷' :
             input.inputArr.push(' ' + str + ' ');
         break;
         default :
         input.inputArr.push(str);
     }
-    if (input.inputArr.length === 0) {
-        output.text.innerHTML = "Empty"
+    if (input.empty()) {
+        output.text.innerHTML = "0"
     } else {
-        output.text.innerHTML = input.getInput();
+        output.display();
+        
     }    
     console.log(str);   
-}
+};
 
 var showResult = function(event) {
-    var ret = event.target.innerHTML;
-    console.log(ret);
-}
+    input.prepareCalculate();
+
+    var result = input.getValue();
+            
+    while(!input.empty()) {
+        var op = input.getOperator();        
+        var second = input.getValue();               
+        var result = calculator.calculate(result, second, op);                
+    }
+    output.print(result);
+    input.removeAll(result);
+};
