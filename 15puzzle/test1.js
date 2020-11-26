1f9452a69a0549cecb1c21ab62006e914eb174e1
wonjinsong <msmk22239@gmail.com>
2020-11-26 08:44:53 -0500

    random word display

--- 15puzzle/test1.js
+++ 15puzzle/test1.js
@@ -1,23 +1,35 @@
-let strEl = document.getElementById("str").innerHTML;
+let strEl = document.getElementById("str");
 let mixStrEl = document.getElementById('mixStr');
 let game = {};
-game.word = strEl.split('');
-console.log(game.word.length);
+
+game.words = ['apple', 'javascript', 'react', 'jobsearch', 'linkerdin', 'indeed', 'corona', 'blue', 'jenny', 'daniel', 'mason', 'korea'];
+game.word = [];
 game.btns = [];
+game.addButtons = function() {
+    for (var i = 0; i < this.word.length; i++){
+        let btn = document.createElement('button');
+        btn.innerHTML = this.word[i];
+        mixStrEl.appendChild(btn);
+        this.btns.push(btn);
+    }
+};
 
 game.choiceText = function(){
     for(var i =0; i < this.word.length; i++) {
         this.btns[i].innerHTML = this.word[i];
     }
 };
-
-for (var i = 0; i < strEl.length; i++) {
-    let btn = document.createElement('button');
-    btn.innerHTML = strEl[i];
-    mixStrEl.appendChild(btn);
-    game.btns.push(btn);
-    console.log(game.btns);
-}
+game.randomWord = function(){
+    let idx = Math.floor(Math.random() * this.words.length);
+    let s =game.words[idx];
+    strEl.innerHTML =s;
+    this.word = s.split('');
+    console.log(this.word);
+    //this.choiceText();
+};
+game.randomWord();
+game.addButtons();
+game.choiceText();
 
 flip = (event) => {
     let temp = [];
